0688a42af209fe9639d03fbad25ab96bc93faf7e
wangfei <785509893@qq.com>
2020-06-05 17:27:09 +0800

    2

---
+++ src/components/goods/Cate.vue
@@ -0,0 +1,157 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加分类</el-button>
+        </el-col>
+      </el-row>
+      <!-- 表格区 -->
+      <tree-table
+        class="tree-table"
+        ref="table"
+        sum-text="sum"
+        index-text="#"
+        :data="catelist"
+        :columns="columns"
+        :stripe="props.stripe"
+        :border="props.border"
+        :show-header="props.showHeader"
+        :show-summary="props.showSummary"
+        :show-row-hover="props.showRowHover"
+        :show-index="props.showIndex"
+        :tree-type="props.treeType"
+        :is-fold="props.isFold"
+        :expand-type="props.expandType"
+        :selection-type="props.selectionType"
+      >
+        <template slot="isok" slot-scope="scope">
+          <i
+            class="el-icon-success"
+            v-if="scope.row.cat_deleted == false"
+            style="color: lightgreen;"
+          ></i>
+          <i class="el-icon-error" style="color: red;" v-else></i>
+        </template>
+        <template slot="order" slot-scope="scope">
+          <el-tag size="mini" v-if="scope.row.cat_level == 0">一级</el-tag>
+          <el-tag size="mini" v-else-if="scope.row.cat_level == 1" type="success">二级</el-tag>
+          <el-tag size="mini" v-else type="warning">三级</el-tag>
+        </template>
+        <template slot="opt">
+          <el-button size="mini" type="primary">编辑</el-button>
+          <el-button size="mini" type="danger">删除</el-button>
+        </template>
+      </tree-table>
+      <!-- 分页区 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="querInfo.pagenum"
+        :page-sizes="[3, 5, 10, 15]"
+        :page-size="querInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 查询条件
+      querInfo: {
+        type: 3,
+        pagenum: 1,
+        pagesize: 5
+      },
+      // 商品分类的数据列表，默认为空
+      catelist: [],
+      // 总数据条数
+      total: 0,
+      props: {
+        stripe: false,
+        border: true,
+        showHeader: true,
+        showSummary: false,
+        showRowHover: false,
+        showIndex: true,
+        treeType: true,
+        isFold: true,
+        expandType: false,
+        selectionType: false
+      },
+      // 为table指定列的定义
+      columns: [
+        {
+          label: '分类名称',
+          prop: 'cat_name'
+        },
+        {
+          label: '是否有效',
+          // 表示，将当前列定义为模板列
+          type: 'template',
+          // 表示当前这一列使用模板名称
+          template: 'isok'
+        },
+        {
+          label: '排序',
+          // 表示，将当前列定义为模板列
+          type: 'template',
+          // 表示当前这一列使用模板名称
+          template: 'order'
+        },
+        {
+          label: '操作',
+          // 表示，将当前列定义为模板列
+          type: 'template',
+          // 表示当前这一列使用模板名称
+          template: 'opt'
+        }
+      ]
+    }
+  },
+  created() {
+    this.getCateList()
+  },
+  methods: {
+    async getCateList() {
+      const { data: res } = await this.$http.get('categories', {
+        params: this.querInfo
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品分类失败！')
+      }
+
+      console.log(res.data)
+      // 把数据列表，赋值给 catelist
+      this.catelist = res.data.result
+      // 为总数据条数赋值
+      this.total = res.data.total
+    },
+    handleSizeChange(newSize) {
+      this.querInfo.pagesize = newSize
+      this.getCateList()
+    },
+    handleCurrentChange(newPage) {
+      this.querInfo.pagenum = newPage
+      this.getCateList()
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.tree-table {
+  margin-top: 15px;
+}
+</style>
